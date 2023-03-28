84780ce43d8cc22396373e66c1ac2de461eed5e7
HenriqueLRBarbosa <Rik_lrb@hotmail.com>
2023-03-28 16:09:31 -0300

    Primeiro Commit-Projeto iphone

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function trocaCor(cor){
+    let circulo =document.querySelector(".circulo")
+    circulo.style.background = cor
+}
+
+function trocarImagem(imagem){
+    let imgiphone = document.querySelector(".iphone")
+    imgiphone.src = imagem
+}
